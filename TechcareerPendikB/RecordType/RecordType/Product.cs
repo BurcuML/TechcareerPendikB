c3f34a225724c747e151396a61816db7a64faa47
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2024-10-03 16:24:40 +0300

    Proje dosyası ekle.

---
+++ TechcareerPendikB/RecordType/RecordType/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordType
+{
+    public record Product(string Name, string Description, string Stock, string Price, Category category);
+
+   
+}
---
+++ TechcareerPendikB/RecordType/RecordType/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RecordType;
+
+Console.WriteLine("Hello, World!");
+
+Category category = new Category(Id: 1, Name: "Random", Description: "ksdjhfkjdsdjsjssdj");
+Product product = new Product
+    (
+    Name: "dkjfkdj", 
+    Description: "kdjnkjnd", 
+    Stock: "kjdnvkjnv", 
+    Price: "dlfknvjdfnv",
+    Category: category,
+    );
+Console.WriteLine(product);
+
+//Category category = new Category(Id: 1, Name: "Random", Description: "ksdjhfkjdsdjsjssdj");
+//Category category1 = new Category(Id: 2, Name: "Randomc", Description: "ksdjhfkjdsdjsjssdj", Product: product);
+//Console.WriteLine(category);
+//Console.WriteLine(category1);
+// generics => iş yükümüz değişiyor ve diğer tüm her şey aynıysa kullanıyoruz
